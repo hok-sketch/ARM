3d07bcbf84997948a3577ee9a07041b359e178d3
hok-sketch <hokoper@gmail.com>
2025-08-21 00:23:39 +0300

    Delete src directory

--- src/components/ui/card.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function Card({ className = '', children }) {
-  return <div className={`rounded-2xl border bg-white shadow-sm ${className}`}>{children}</div>
-}
-export function CardContent({ className = '', children }) {
-  return <div className={`p-6 ${className}`}>{children}</div>
-}
