03725a22255dc0d205c9fb5dd3468a1eee48a6ef
hok-sketch <hokoper@gmail.com>
2025-08-21 00:23:19 +0300

    Delete ARM directory

--- ARM/src/components/ui/input.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Input(props) {
-  return <input {...props} className={['h-11 px-4 border rounded-2xl outline-none focus:ring-2 focus:ring-slate-300', props.className].join(' ')} />
-}
--- ARM/src/components/ui/textarea.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Textarea(props) {
-  return <textarea {...props} className={['p-4 border rounded-2xl outline-none focus:ring-2 focus:ring-slate-300', props.className].join(' ')} />
-}
